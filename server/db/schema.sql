14716852a64355481a492a3ba232aec1f96628cb
nicgen <nic@genty.dev>
2025-02-28 00:38:25 +0100

    import sql and create db if missing

---
+++ server/db/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS USERS
+(
+    id            INTEGER PRIMARY KEY AUTOINCREMENT,
+    nickname      TEXT NOT NULL,
+    age           INTEGER,
+    gender        TEXT,
+    first_name    TEXT,
+    last_name     TEXT,
+    email         TEXT NOT NULL UNIQUE,
+    password      TEXT NOT NULL,
+    date_register TEXT NOT NULL
+);
