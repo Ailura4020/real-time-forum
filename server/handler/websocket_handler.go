86fb4499b31950b4518956a93fac22b8fbcc1031
F-justine <justine.faure98@outlook.com>
2025-03-04 15:33:39 +0100

    privateMessage

--- server/handler/websocket_handler.go
+++ server/handler/websocket_handler.go
@@ -11,21 +11,27 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// Config de l'upgrader WebSocket pour accepter toutes les origines
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
+// Structure d'un client connecté via WebSocket
 type Client struct {
 	Conn     *websocket.Conn
 	Username string
 	UserId   int
 }
 
+// Map pour stocker les clients connectés
 var clients = make(map[*Client]bool)
+
+// var pour la connexion la db
 var db *sql.DB
 
+// function gestion des connexions WebSocket
 func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -34,15 +40,18 @@ func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
+	// création d'un nouveau client
 	client := &Client{
 		Conn:     conn,
 		Username: "user1",
 		UserId:   1,
 	}
 
+	// ajout du client à la map
 	clients[client] = true
 	fmt.Println("client connecté", client.Username)
 
+	// boucle lecture d'un message client
 	for {
 		messageType, msg, err := conn.ReadMessage()
 		if err != nil {
@@ -56,13 +65,13 @@ func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Erreur lors de la désérialisation du message", err)
 			continue
 		}
-
+		// enregistre le message dans la db
 		if err := savePrivateMessage(db, client.UserId, message.ReceiverID, message.Content); err != nil {
 			fmt.Println("Erreur lors de l'enregistrement du message", err)
 		}
-
 		// fmt.Printf("Message reçu : %s\n", msg)
 
+		// envoie le message au destinataire
 		for c := range clients {
 			if c.UserId == message.ReceiverID {
 				err := c.Conn.WriteMessage(messageType, msg)
@@ -77,6 +86,7 @@ func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// function pour enregistrer un msg privé dans la DB
 func savePrivateMessage(db *sql.DB, senderID int, receiverId int, content string) error {
 	dateSent := time.Now().Format(time.RFC3339)
 	query := `INSERT INTO PRIVATEMESSAGE (TextContent, DateSent, SenderId, ReceiverId) VALUE ( ?, ?, ?, ?)`
