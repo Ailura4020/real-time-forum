64b99bde55ef35761fa3b3d5ffd77f9624bbe522
F-justine <justine.faure98@outlook.com>
2025-04-10 13:28:51 +0200

    Websocket.js en cours

--- front/src/main.js
+++ front/src/main.js
@@ -20,15 +20,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // Check if user is already logged in
     const token = localStorage.getItem('token');
-    // const userData = localStorage.getItem('userData');
-
-    // if (token && userData) {
-    //     // Update UI for logged in user
-    //     const userDataObj = JSON.parse(userData);
-    //     updateAuthUI(userDataObj);
-    //
-    // }
-
+  
     if (token) {
         // Fetch user data
         try {
@@ -68,7 +60,7 @@ export async function updateAuthUI() {
     if (token) {
         userData = await fetchUserData();
         if (userData) {
-            connectWebSocket()
+            connectWebSocket(updateConnectedUsers)
             // console.log("[SUCCESS]",userData, userData.data.nickname);
             // User is logged in
             authContainer.innerHTML = `
