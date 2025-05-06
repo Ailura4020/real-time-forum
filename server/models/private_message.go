316eb5f36e27172e3593adef73be3a833806e214
nicgen <nic@genty.dev>
2025-05-06 16:37:07 +0200

    correct privatemessage handler

--- server/models/private_message.go
+++ server/models/private_message.go
@@ -1,16 +1,18 @@
 package models
 
-import (
-	"database/sql"
-	"encoding/json"
-	"net/http"
-	"real-time-forum/repository"
-	"real-time-forum/utils"
-	"strconv"
-	"time"
+import "time"
 
-	"github.com/gorilla/mux"
-)
+//import (
+//	"database/sql"
+//	"encoding/json"
+//	"net/http"
+//	"real-time-forum/repository"
+//	"real-time-forum/utils"
+//	"strconv"
+//	"time"
+//
+//	"github.com/gorilla/mux"
+//)
 
 type PrivateMessage struct {
 	ID         int       `json:"id"`
@@ -20,36 +22,36 @@ type PrivateMessage struct {
 	Timestamp  time.Time `json:"timestamp"`
 }
 
-func GetMessagesHandler(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		userID, err := utils.ExtractUserIDFromRequest(r)
-		if err != nil {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			return
-		}
-
-		vars := mux.Vars(r)
-		receiverIDStr, ok := vars["receiverId"]
-		if !ok {
-			http.Error(w, "Missing receiver ID", http.StatusBadRequest)
-			return
-		}
-
-		receiverID, err := strconv.Atoi(receiverIDStr)
-		if err != nil {
-			http.Error(w, "Invalid receiver ID", http.StatusBadRequest)
-			return
-		}
-
-		messages, err := repository.GetConversationMessages(db, userID, receiverID)
-		if err != nil {
-			http.Error(w, "Failed to fetch messages", http.StatusInternalServerError)
-			return
-		}
-
-		json.NewEncoder(w).Encode(messages)
-	}
-}
+//func GetMessagesHandler(db *sql.DB) http.HandlerFunc {
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		userID, err := utils.ExtractUserIDFromRequest(r)
+//		if err != nil {
+//			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+//			return
+//		}
+//
+//		vars := mux.Vars(r)
+//		receiverIDStr, ok := vars["receiverId"]
+//		if !ok {
+//			http.Error(w, "Missing receiver ID", http.StatusBadRequest)
+//			return
+//		}
+//
+//		receiverID, err := strconv.Atoi(receiverIDStr)
+//		if err != nil {
+//			http.Error(w, "Invalid receiver ID", http.StatusBadRequest)
+//			return
+//		}
+//
+//		messages, err := repository.GetConversationMessages(db, userID, receiverID)
+//		if err != nil {
+//			http.Error(w, "Failed to fetch messages", http.StatusInternalServerError)
+//			return
+//		}
+//
+//		json.NewEncoder(w).Encode(messages)
+//	}
+//}
 
 // func GetMessagesHandler(db *sql.DB) http.HandlerFunc {
 // 	return func(w http.ResponseWriter, r *http.Request) {
